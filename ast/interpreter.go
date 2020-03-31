7ba0fbb995c0f2821a7d3be1b56679c4fedb0bf6
Martin Musale <martinmshale@gmail.com>
2020-03-31 10:21:48 +0300

    Add the implementation for visiting the basic expressions

--- ast/interpreter.go
+++ ast/interpreter.go
@@ -1,7 +1,32 @@
 package ast
 
+import (
+	"fmt"
+	"lo/parseerror"
+	"lo/token"
+	"reflect"
+)
+
 // Interpreter ...
 type Interpreter struct{}
+
+// NewInterpreter creates a new interpreter
+func NewInterpreter() *Interpreter {
+	return &Interpreter{}
+}
+
+// Interpret the given expressions
+func (i Interpreter) Interpret(e Expr) {
+	value := i.evaluate(e)
+	fmt.Sprint(value)
+}
+
+func (i *Interpreter) String() string {
+	if i == nil {
+		return "nil"
+	}
+	return i.String()
+}
 
 // VisitAssignExpression ...
 func (i Interpreter) VisitAssignExpression(e *AssignExpr) interface{} {
@@ -10,7 +35,79 @@ func (i Interpreter) VisitAssignExpression(e *AssignExpr) interface{} {
 
 // VisitBinaryExpression ...
 func (i Interpreter) VisitBinaryExpression(e *BinaryExpr) interface{} {
-	return ""
+	left := i.evaluate(e.Left)
+	right := i.evaluate(e.Right)
+
+	switch e.Operator.Type {
+	case token.MINUS:
+		i.checkOneNumberOperand(e.Operator, right)
+		return left.(float64) - right.(float64)
+	case token.SLASH:
+		i.checkTwoNumberOperands(e.Operator, left, right)
+		return left.(float64) / right.(float64)
+	case token.STAR:
+		i.checkTwoNumberOperands(e.Operator, left, right)
+		return left.(float64) * right.(float64)
+	case token.GREATER:
+		i.checkTwoNumberOperands(e.Operator, left, right)
+		return left.(float64) > right.(float64)
+	case token.GREATEREQUAL:
+		i.checkTwoNumberOperands(e.Operator, left, right)
+		return left.(float64) >= right.(float64)
+	case token.LESS:
+		i.checkTwoNumberOperands(e.Operator, left, right)
+		return left.(float64) < right.(float64)
+	case token.LESSEQUAL:
+		i.checkTwoNumberOperands(e.Operator, left, right)
+		return left.(float64) <= right.(float64)
+	case token.BANGEQUAL:
+		return !i.isEqual(left, right)
+	case token.EQUALEQUAL:
+		return i.isEqual(left, right)
+	case token.PLUS:
+		typeLeft := reflect.TypeOf(left).String()
+		typeRight := reflect.TypeOf(right).String()
+		if (typeLeft == "float64" || typeLeft == "float32" || typeLeft == "int") && (typeRight == "float64" || typeRight == "float32" || typeRight == "int") {
+			return left.(float64) + right.(float64)
+		} else if typeLeft == "string" && typeRight == "string" {
+			return left.(string) + right.(string)
+		}
+		return &parseerror.RunTimeError{
+			Token:   e.Operator,
+			Message: fmt.Sprintf("Operand %v and %v must be numbers or strings", left, right)}
+	}
+	return nil
+}
+
+// checkOneNumberOperand if it's a float or int
+func (i Interpreter) checkOneNumberOperand(operator token.Token, operand interface{}) error {
+	typeOfOperand := reflect.TypeOf(operand).String()
+	if typeOfOperand == "float64" || typeOfOperand == "float32" || typeOfOperand == "int" {
+		return nil
+	}
+	return &parseerror.RunTimeError{Token: operator, Message: fmt.Sprintf("Operand %v must be a number", operand)}
+}
+
+// checkTwoNumberOperands if they are floats or ints
+func (i Interpreter) checkTwoNumberOperands(operator token.Token, left interface{}, right interface{}) error {
+	typeOfLeftOperand := reflect.TypeOf(left).String()
+	typeOfRightOperand := reflect.TypeOf(right).String()
+	if (typeOfLeftOperand == "float64" || typeOfLeftOperand == "float32" || typeOfLeftOperand == "int") &&
+		(typeOfRightOperand == "float64" || typeOfRightOperand == "float32" || typeOfRightOperand == "int") {
+		return nil
+	}
+	return &parseerror.RunTimeError{Token: operator, Message: fmt.Sprintf("Operand %v and %v must be a number", left, right)}
+}
+
+// isEqual returns true if 2 objects are the same
+func (i Interpreter) isEqual(left interface{}, right interface{}) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil {
+		return false
+	}
+	return reflect.DeepEqual(left, right)
 }
 
 // VisitCallExpression ...
@@ -23,14 +120,20 @@ func (i Interpreter) VisitGetExpression(e *GetExpr) interface{} {
 	return ""
 }
 
-// VisitGroupExpression ...
+// VisitGroupExpression resturns the result of values in parenthesis
+// expression
 func (i Interpreter) VisitGroupExpression(e *GroupExpr) interface{} {
-	return ""
+	return i.evaluate(e.Expression)
 }
 
-// VisitLiteralExpression ...
+// evaluate is a helper that revisits the interpretor
+func (i Interpreter) evaluate(e Expr) interface{} {
+	return e.Accept(i)
+}
+
+// VisitLiteralExpression returns the runtime value the parser took
 func (i Interpreter) VisitLiteralExpression(e *LiteralExpr) interface{} {
-	return ""
+	return e.Object
 }
 
 // VisitLogicalExpression ...
@@ -50,7 +153,30 @@ func (i Interpreter) VisitThisExpression(e *ThisExpr) interface{} {
 
 // VisitUnaryExpression ...
 func (i Interpreter) VisitUnaryExpression(e *UnaryExpr) interface{} {
-	return ""
+	right := i.evaluate(e.Right)
+	switch e.Operator.Type {
+	case token.MINUS:
+		value := -right.(float64)
+		return value
+	case token.BANG:
+		return !i.isTruthy(true)
+	}
+	return nil
+}
+
+// isTruthy returns false and nil object as falsey and everything else as
+// truthy
+func (i Interpreter) isTruthy(obj interface{}) bool {
+	if obj == nil {
+		return false
+	}
+	kind := reflect.TypeOf(obj).Kind()
+	switch kind {
+	case reflect.Bool:
+		return obj.(bool)
+	default:
+		return true
+	}
 }
 
 // VisitVariableExpression ...
